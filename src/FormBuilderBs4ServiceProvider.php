7bf40730b3e0488cad4712b8b389695797d953fd
Stéphane Rouillé <steph.rouille@gmail.com>
2021-03-10 12:18:18 +0100

    Initial commit (clone from  ycs77/laravel-form-builder-bs4 )

---
+++ src/FormBuilderBs4ServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Stfr\LaravelFormBuilderTailwind2;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormBuilderTailwind2ServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/laravel-form-builder.php' => config_path('laravel-form-builder.php'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-form-builder'),
+        ], 'laravel-form-builder-tailwind2');
+
+        $this->publishes([
+            __DIR__ . '/../config/laravel-form-builder-horizontal.php' => config_path('laravel-form-builder.php'),
+            __DIR__ . '/../resources/views-horizontal' => resource_path('views/vendor/laravel-form-builder'),
+        ], 'laravel-form-builder-tailwind2-horizontal');
+    }
+}
